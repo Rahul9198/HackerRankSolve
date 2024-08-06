ea4b0846475799a6c9240680b1bed3d6ec3876aa
RAHUL KUMAR VERMA <111939912+Rahul9198@users.noreply.github.com>
2024-08-06 11:48:43 +0530

    Add files via upload

---
+++ HakerRank/Welcome.java
@@ -0,0 +1,11 @@
+// import java.io.*;
+// import java.util.*;
+
+public class Welcome {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+       
+    }
+}
---
+++ HakerRank/loops1.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class loops1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        for(int i = 1 ; i<=10;i++){
+            int ans = N * i;
+            System.out.println(N+ " x " +i+" = "+ans);
+        }
+        bufferedReader.close();
+    }
+    
+}
